a938ee70da15b3edc539674b57a47073a7ce62ae
jmunarb <munar.bestard.joan@gmail.com>
2016-11-15 17:10:42 +0100

    Upload fet

--- src/main/Java/UploadServlet.java
+++ src/main/Java/UploadServlet.java
@@ -47,7 +47,7 @@ public class UploadServlet extends HttpServlet {
         // maximum size that will be stored in memory
         factory.setSizeThreshold(maxMemSize);
         // Location to save data that is larger than maxMemSize.
-        factory.setRepository(new File("c:\\temp"));
+        factory.setRepository(new File(filePath));
 
         // Create a new file upload handler
         ServletFileUpload upload = new ServletFileUpload(factory);
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,15 @@
 <html>
+<head>
+<title>File Uploading Form</title>
+</head>
 <body>
-<h2>Hello World!</h2>
+<h3>File Upload:</h3>
+Select a file to upload: <br />
+<form action="UploadServlet" method="post" enctype="multipart/form-data">
+<input type="file" name="file" size="100" />
+<br />
+<br />
+<input type="submit" value="Upload File" />
+</form>
 </body>
 </html>
